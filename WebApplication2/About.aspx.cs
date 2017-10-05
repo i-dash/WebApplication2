38dd49a931ab078035badbf43cb8ffa989a828ba
Ina Dash <dashing2u@gmail.com>
2017-10-05 02:46:57 -0700

    Making some changes

--- WebApplication2/About.aspx.cs
+++ WebApplication2/About.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApplication2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Write("Making modifications server side.");
         }
     }
 }
